4080215777a13f149c2871b73a8d03912e836002
yjh4507 <yyjh4507@naver.com>
2021-12-01 17:01:02 +0900

    2021-12-01

---
+++ js/main_script.js
@@ -0,0 +1,6 @@
+const toggleBtn = document.querySelector('.navicon');
+const menu = document.querySelector('.nav2');
+
+toggleBtn.addEventListener('click', ()=> {
+    menu.classList.toggle('active');
+});
