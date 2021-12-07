d05570551fc6c5eb2bb20e459ce6e4ba3d0c420e
yjh4507 <93234827+yjh4507@users.noreply.github.com>
2021-12-07 18:02:53 +0900

    2021-12-07

---
+++ js/main_script_mainnav.js
@@ -0,0 +1,33 @@
+const toggleBtn = document.querySelector('.navicon');
+const menu = document.querySelector('.nav2');
+
+toggleBtn.addEventListener('click', ()=> {
+    menu.classList.toggle('active');
+});
+
+
+$(function () {
+    $("#top1").fadeIn();
+    $("#top2").hide();
+    $('#menu li:nth-of-type(1)').click(function () {
+        $("#top1").fadeIn();
+        $("#top2").hide();
+    });
+    $('#menu li:nth-of-type(2)').click(function () {
+        $("#top1").hide();
+        $("#top2").fadeIn();
+    });
+});
+$(function () {
+    $("#middle_section1").fadeIn();
+    $("#middle_section2").hide();
+    $('#menu li:nth-of-type(3)').click(function () {
+        $("#middle_section1").fadeIn();
+        $("#middle_section2").hide();
+    });
+    $('#menu li:nth-of-type(4)').click(function () {
+        $("#middle_section1").hide();
+        $("#middle_section2").fadeIn();
+    });
+});
+
