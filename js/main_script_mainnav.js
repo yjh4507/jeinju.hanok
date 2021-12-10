be6b22b0181e6e41d50dc829350aa5177973d294
yjh4507 <93234827+yjh4507@users.noreply.github.com>
2021-12-10 15:55:42 +0900

    2021-12-10

--- js/main_script_mainnav.js
+++ js/main_script_mainnav.js
@@ -6,28 +6,3 @@ toggleBtn.addEventListener('click', ()=> {
 });
 
 
-$(function () {
-    $("#top1").fadeIn();
-    $("#top2").hide();
-    $('#menu li:nth-of-type(1)').click(function () {
-        $("#top1").fadeIn();
-        $("#top2").hide();
-    });
-    $('#menu li:nth-of-type(2)').click(function () {
-        $("#top1").hide();
-        $("#top2").fadeIn();
-    });
-});
-$(function () {
-    $("#middle_section1").fadeIn();
-    $("#middle_section2").hide();
-    $('#menu li:nth-of-type(3)').click(function () {
-        $("#middle_section1").fadeIn();
-        $("#middle_section2").hide();
-    });
-    $('#menu li:nth-of-type(4)').click(function () {
-        $("#middle_section1").hide();
-        $("#middle_section2").fadeIn();
-    });
-});
-
